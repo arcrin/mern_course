fab58aa4ef1453fd46aab21538d77e73a0c2686a
wbai <wbai@deltacontrols.com>
2021-04-27 15:53:29 -0700

    cp4 custom error handling

---
+++ backend/server.js
@@ -0,0 +1,38 @@
+import express from "express"
+
+import dotenv from 'dotenv'
+import connectDB from "./config/db.js"
+import productRoute from "./routes/productRoutes.js";
+import colors from 'colors'
+
+dotenv.config()
+
+connectDB()
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('API is running...')
+})
+
+app.use('/api/products', productRoute)
+
+app.use((req, res, next) => {
+    const error = new Error(`Not Found - ${req.originalUrl}`)
+    res.status(404)
+    next(error)
+})
+
+app.use((err, req, res, next) => {
+    const statusCode = res.statusCode === 200 ? 500 : res.statusCode
+    res.status(statusCode)
+    res.json({
+        message: err.message,
+        stack: process.env.NODE_ENV === "production" ? null: err.stack
+    })
+})
+
+const PORT = process.env.PORT || 5000
+const NODE_ENV = process.env.NODE_ENV
+
+app.listen(PORT, console.log(`Server running in ${NODE_ENV} on port ${PORT}`.yellow.bold));
--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,17 +1,24 @@
 import React from 'react';
-import {Navbar, Nav, NavDropdown, FormControl, Form, Button, Container} from "react-bootstrap";
+import { LinkContainer } from 'react-router-bootstrap'
+import {Navbar, Nav, Container} from "react-bootstrap";
 
 const Header = () => {
     return (
         <header>
             <Navbar bg="dark" variant='dark' expand="lg" collapseOnSelect={true}>
                 <Container>
-                    <Navbar.Brand href="/">TAG Dashboard</Navbar.Brand>
+                    <LinkContainer to="/">
+                        <Navbar.Brand>TAG Dashboard</Navbar.Brand>
+                    </LinkContainer>
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
                         <Nav className="ml-auto">
-                            <Nav.Link href="/cart"><i className={'fas fa-shopping-cart'}/>Cart</Nav.Link>
-                            <Nav.Link href="/login"><i className={'fas fa-user'}/>Sign In</Nav.Link>
+                            <LinkContainer to="/cart">
+                                <Nav.Link ><i className={'fas fa-shopping-cart'}/>Cart</Nav.Link>
+                            </LinkContainer>
+                            <LinkContainer to="/login">
+                                <Nav.Link ><i className={'fas fa-user'}/>Sign In</Nav.Link>
+                            </LinkContainer>
                             {/*<NavDropdown title="Dropdown" id="basic-nav-dropdown">*/}
                             {/*    <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>*/}
                             {/*    <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>*/}
