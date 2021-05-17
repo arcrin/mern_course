29449fac8667af741332f29e924b30cfe9ff5f75
wbai <wbai@deltacontrols.com>
2021-05-17 13:49:40 -0700

    need to figure out what is wrong with productRoutes.js

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,44 +1,20 @@
 import express from "express";
 import asyncHandler from "express-async-handler";
-import Product from "../models/productModel.js";
-import mongodb from "mongodb";
-import dotenv from 'dotenv'
-
-dotenv.config()
-
 const router = express.Router()
+import Product from "../models/productModel.js";
 
-const MongoClient = mongodb.MongoClient
+router.get('/', asyncHandler(async (req, res) => {
+    const products = await Product.find({})
+    res.json(products)
+}))
 
-const oid = mongodb.ObjectID
-
-// @desc    Fetch all products
-// @route   GET /api/products
-// @access  Public
-router.get('/', (req, res, next) => {
-    MongoClient.connect(process.env.MONGO_URI, (err, client) => {
-        const collection = client.db(process.env.MONGO_DB).collection(process.env.MONGO_COLLECTION)
-        collection.find().toArray(function(err, result){
-            res.json(result)
-        })
-        client.close()
-    })
-})
-
-
-// @desc    Fetch single product
-// @route   GET /api/products/:id
-// @access  Public
-router.get('/:id',  (req, res) => {
-    MongoClient.connect(process.env.MONGO_URI, (err, client) => {
-        const collection = client.db(process.env.MONGO_DB).collection(process.env.MONGO_COLLECTION)
-        collection.findOne({_id: oid(req.params.id)}, (err, result) => {
-            res.json(result)
-        })
-        client.close()
-    })
-})
-
-
+router.get('/:id', asyncHandler(async (req, res) => {
+    const product = await Product.findById(req.params.id)
+    if (product) {
+        res.json(product)
+    } else {
+        res.status(404).json({message: 'Product not found'})
+    }
+}))
 
 export default router
